c8d1b2861528e5da7a18ff55a21201e29c5bafbc
TrangBN <trangbn580@gmail.com>
2019-05-19 09:20:42 +0700

    commit login page

--- src/main/java/com/fullstack/controller/backend/AuthController.java
+++ src/main/java/com/fullstack/controller/backend/AuthController.java
@@ -18,10 +18,6 @@ public class AuthController  {
     public String actionLoginProcess() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof UserDetails ) {
-
-//            && "admin".equals(((UserDetails)principal).getUsername())
-//                    && "adminPass".equals(((UserDetails)principal).getPassword())
-//            return "backend/layouts/main";
             return "backend/login";
         }
         return "backend/layouts/main";
@@ -34,7 +30,6 @@ public class AuthController  {
 
     @RequestMapping(value = "/logout.html", method = RequestMethod.GET)
     public String actionLogout() {
-//        return "backend/login";
-        return "backend/layouts/main";
+        return "backend/login";
     }
 }
